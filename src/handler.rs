48342aa6ee3789e26290b76774a9229a8d75cc8b
Scott Abbott <scott.abbott.a@gmail.com>
2023-05-24 17:27:19 -0600

    Simplify for_all_tuples macro

--- src/handler.rs
+++ src/handler.rs
@@ -4,23 +4,19 @@ pub trait Handler<T> {
 
 macro_rules! impl_handler {
     (
-        [$($ty:ident),*], $last:ident
+        $($ty:ident),*
     ) => {
         #[allow(non_snake_case, unused_mut)]
-        impl<F, $($ty,)* $last> Handler<($($ty,)* $last,)> for F
+        impl<Fn, $($ty,)*> Handler<($($ty,)*)> for Fn
         where
-            F: FnOnce($($ty,)* $last,) + 'static,
+            Fn: FnOnce($($ty,)*) + 'static,
             $( $ty: crate::FromConsumerState<Error = String>, )*
-            $last: crate::FromConsumerState<Error = String>,
         {
             fn call(self, message_data: crate::MessageData) {
                 $(
                     let $ty = $ty::from_consumer_state(message_data.clone()).unwrap();
                 )*
-
-                let $last = $last::from_consumer_state(message_data.clone()).unwrap();
-
-                self($($ty,)* $last,);
+                self($($ty,)*);
             }
         }
     };
@@ -28,23 +24,23 @@ macro_rules! impl_handler {
 
 #[rustfmt::skip]
 macro_rules! for_all_tuples {
-    ($name:ident) => {
-        $name!([], T1);
-        $name!([T1], T2);
-        $name!([T1, T2], T3);
-        $name!([T1, T2, T3], T4);
-        $name!([T1, T2, T3, T4], T5);
-        $name!([T1, T2, T3, T4, T5], T6);
-        $name!([T1, T2, T3, T4, T5, T6], T7);
-        $name!([T1, T2, T3, T4, T5, T6, T7], T8);
-        $name!([T1, T2, T3, T4, T5, T6, T7, T8], T9);
-        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9], T10);
-        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10], T11);
-        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11], T12);
-        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12], T13);
-        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13], T14);
-        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14], T15);
-        $name!([T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15], T16);
+    ($macro_name:ident) => {
+        $macro_name!(T1);
+        $macro_name!(T1, T2);
+        $macro_name!(T1, T2, T3);
+        $macro_name!(T1, T2, T3, T4);
+        $macro_name!(T1, T2, T3, T4, T5);
+        $macro_name!(T1, T2, T3, T4, T5, T6);
+        $macro_name!(T1, T2, T3, T4, T5, T6, T7);
+        $macro_name!(T1, T2, T3, T4, T5, T6, T7, T8);
+        $macro_name!(T1, T2, T3, T4, T5, T6, T7, T8, T9);
+        $macro_name!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10);
+        $macro_name!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11);
+        $macro_name!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12);
+        $macro_name!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13);
+        $macro_name!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14);
+        $macro_name!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15);
+        $macro_name!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16);
     };
 }
 
