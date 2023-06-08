51ce4d0e90963c94e02fab0accd576c41fc30395
Scott Abbott <scott.abbott.a@gmail.com>
2023-06-08 13:40:06 -0600

    Write open

---
+++ examples/account_component/src/bin/write_open.rs
@@ -0,0 +1,39 @@
+use account_component::{messages::commands::Open, AccountCommandCategory};
+use aqueous::*;
+use std::{error::Error, env, collections::VecDeque, str::FromStr};
+use time::OffsetDateTime;
+use uuid::Uuid;
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn Error>> {
+
+    let pool = sqlx::postgres::PgPoolOptions::new()
+        .connect("postgres://message_store@localhost/message_store")
+        .await?;
+
+    let mut args: VecDeque<String> = env::args().collect();
+
+    let account_id = args.pop_front()
+        .and_then(|arg| Uuid::from_str(&arg).ok())
+        .unwrap_or_else(|| Uuid::new_v4());
+
+    let open = Open {
+        account_id,
+        customer_id: Uuid::new_v4(),
+        time: OffsetDateTime::now_utc(),
+    };
+
+    let AccountCommandCategory(category) = AccountCommandCategory::new();
+
+    let stream_id = StreamID::new(open.account_id);
+    let stream_name = category.stream_name(stream_id);
+
+    println!("Writing {:#?}", open);
+
+    WriteMessages::new(pool.clone())
+        .with_message(open)
+        .execute(stream_name)
+        .await?;
+
+    Ok(())
+}
