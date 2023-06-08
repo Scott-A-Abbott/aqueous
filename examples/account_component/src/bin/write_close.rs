251b1c53b319248fc8d575d86c6cafbfd05369a7
Scott Abbott <scott.abbott.a@gmail.com>
2023-06-08 13:41:33 -0600

    Write close

---
+++ examples/account_component/src/bin/write_close.rs
@@ -0,0 +1,38 @@
+use account_component::{messages::commands::Close, AccountCommandCategory};
+use aqueous::*;
+use std::{error::Error, env, collections::VecDeque, str::FromStr};
+use time::OffsetDateTime;
+use uuid::Uuid;
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn Error>> {
+
+    let pool = sqlx::postgres::PgPoolOptions::new()
+        .connect("postgres://message_store@localhost/message_store")
+        .await?;
+
+    let mut args: VecDeque<String> = env::args().collect();
+
+    let account_id = args.pop_front()
+        .and_then(|arg| Uuid::from_str(&arg).ok())
+        .unwrap_or_else(|| Uuid::new_v4());
+
+    let close = Close {
+        account_id,
+        time: OffsetDateTime::now_utc(),
+    };
+
+    let AccountCommandCategory(category) = AccountCommandCategory::new();
+
+    let stream_id = StreamID::new(close.account_id);
+    let stream_name = category.stream_name(stream_id);
+
+    println!("Writing {:#?}", close);
+
+    WriteMessages::new(pool.clone())
+        .with_message(close)
+        .execute(stream_name)
+        .await?;
+
+    Ok(())
+}
